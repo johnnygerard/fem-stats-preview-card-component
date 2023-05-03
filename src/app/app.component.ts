0af32af9b82e608d308e6dd39295006409113e0d
Johnny Gérard <john@jgerard.dev>
2023-05-03 15:09:56 +0200

    feat: hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fem-stats-preview-card-component';
+  protected readonly ikBaseURL = `https://ik.imagekit.io/jgerard/${this.title}`;
 }
