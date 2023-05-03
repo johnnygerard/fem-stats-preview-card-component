f95c6785f926426e76fd474e37077739d9fe2118
Johnny Gérard <john@jgerard.dev>
2023-05-03 08:32:42 +0200

    feat: image optimization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgOptimizedImage, provideImageKitLoader } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +9,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgOptimizedImage
   ],
-  providers: [],
+  providers: [
+    provideImageKitLoader('https://ik.imagekit.io/jgerard/fem-stats-preview-card-component')
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
